e67519240038c989ea5cd7b10538eb7b5d2c918a
kmunene <kmunene529@gmail.com>
2024-06-08 03:31:37 +0300

    deleted files

--- 1-get_list_student_ids.js
+++
@@ -1,6 +0,0 @@
-export default function getListStudentIds(students) {
-  if (!Array.isArray(students)) {
-    return [];
-  }
-  return students.map((std) => std.id);
-}
--- 2-get_students_by_loc.js
+++
@@ -1,6 +0,0 @@
-export default function getStudentsByLocation(students, city) {
-  if (!Array.isArray(students)) {
-    return [];
-  }
-  return students.filter((std) => std.location === city);
-}
